269aff4aa6cec1a9a94df29af4e357e718c4f1df
YRibak <yurel83@list.ru>
2015-06-23 19:52:24 +0300

    Lesson 2 materials

---
+++ Hillel/src/lesson2/DataTypes.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+/**
+ * Created by ITHILLEL2 on 23.06.2015.
+ */
+public class DataTypes {
+
+    public static void main(String[] args) {
+        boolean b = false;
+        byte bYTE = 2;
+        int in = 2;
+        long lon = 2;
+        short sh = 3;
+        char ch = 'g';
+        float f = 3.3F;
+        double dou = 3.3;
+
+    }
+}
