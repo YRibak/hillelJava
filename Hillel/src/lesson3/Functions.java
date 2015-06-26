ce4eb377f3499154ebf12fb9e42cfc7660900022
YRibak <yurel83@list.ru>
2015-06-26 20:08:50 +0300

    Lesson 2 materials

--- Hillel/src/lesson3/Functions.java
+++ Hillel/src/lesson3/Functions.java
@@ -12,10 +12,27 @@ public class Functions {
         double result;
         result = add(f,s);
         System.out.println(result);
+        printHello();
+        printMessage("Hello");
     }
+    // функция возвращает обработаное значение
     public static double add (double first, double second)  {
         double sum = first + second;
         return sum;
+    }
+
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+// вызов метода из другого метода
+    public static void printThroughtOtherMethod() {
+       // System.out.println("Hello");
+        printMessage("Hello");
+
     }
 }
 
