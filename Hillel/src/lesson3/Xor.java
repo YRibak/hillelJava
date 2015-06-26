42bc60bc2e75a04529e51c31457de307e25d11bb
YRibak <yurel83@list.ru>
2015-06-26 20:51:03 +0300

    Lesson 2 materials

---
+++ Hillel/src/lesson3/Xor.java
@@ -0,0 +1,34 @@
+package lesson3;
+
+/**
+ * Created by ITHILLEL2 on 26.06.2015.
+ */
+public class Xor {
+    /**
+     * исходные данные
+     * 0101
+     * 0011
+     * логическое и &
+     *
+     * 0001
+     * логическое или |
+     * 0111
+     *
+     * Исключающее или Xor ^
+     * 0110
+     */
+    // для того если надо что-то доделать можно оставлять такие коменты, они напомнят
+//FIXME
+    //TODO implement
+    //---------------------------
+    public static void main(String[] args) {
+        String message = "hello";
+        char kay = 'a';
+    }
+
+    public static String encode(String message, char key){
+
+
+
+    }
+}
