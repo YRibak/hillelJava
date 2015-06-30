1a1e3dc0580ba21a08ee28b21d23e9bb44f1fa0b
YRibak <yurel83@list.ru>
2015-06-30 20:27:31 +0300

    Lesson 2 materials

--- Hillel/src/lesson4/Arrays.java
+++ Hillel/src/lesson4/Arrays.java
@@ -9,6 +9,15 @@ public class Arrays {
         array[2] = 10;
         System.out.println(array[2]);
         // dvuhmerniy massiv
-        int[][] tdArray = new int[][]
+        // int[][] tdArray = new int[8][]; // tak mojno
+        int[][] tdArray = new int[8][];
+        tdArray[0] = new int[10];
+        tdArray[1] = new int[1];  // mojno kagdoy stroke inzializirovat raznoy dlinnu massivi
+
+        // int[][] tdArray = new int[][]; // tak nelzya
+        // int[][] tdArray = new int[8][10]; // tak mojno
+
+
+
     }
 }
