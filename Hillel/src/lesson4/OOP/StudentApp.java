ff9fe6d3f934804c70d80cf03ca17f6602018e23
YRibak <yurel83@list.ru>
2015-06-30 21:15:34 +0300

    Lesson 2 materials

---
+++ Hillel/src/lesson4/OOP/StudentApp.java
@@ -0,0 +1,10 @@
+package lesson4.OOP;
+
+/**
+ * Created by ITHILLEL2 on 30.06.2015.
+ */
+public class StudentApp {
+    public static void main(String[] args) {
+        Students student1 = new Students();
+    }
+}
---
+++ Hillel/src/lesson4/OOP/Students.java
@@ -0,0 +1,11 @@
+package lesson4.OOP;
+
+/**
+ * Created by ITHILLEL2 on 30.06.2015.
+ */
+public class Students {
+    // fields
+    public int id;           // = 0;
+    public String surname;   // = null
+    public int[] marks;      // =null
+}
