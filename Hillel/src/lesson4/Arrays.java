9ec52a759f4722c69f438bedf7c48da9ca4eb0a8
YRibak <yurel83@list.ru>
2015-06-30 20:19:37 +0300

    Lesson 2 materials

---
+++ Hillel/src/lesson4/Arrays.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+/**
+ * Created by ITHILLEL2 on 30.06.2015.
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        int [] array = new int[10];
+        array[2] = 10;
+        System.out.println(array[2]);
+        // dvuhmerniy massiv
+        int[][] tdArray = new int[][]
+    }
+}
