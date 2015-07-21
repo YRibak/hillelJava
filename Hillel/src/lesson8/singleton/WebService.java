f697299d448b1dd8d3e8deaf2d7a6ef5ef06541f
YRibak <yurel83@list.ru>
2015-07-21 19:24:22 +0300

    Lazy Singleton

--- Hillel/src/lesson8/singleton/WebService.java
+++ Hillel/src/lesson8/singleton/WebService.java
@@ -4,10 +4,13 @@ package lesson8.singleton;
  * Created by ITHILLEL2 on 17.07.2015.
  */
 public class WebService {
-    private static WebService service = new WebService();
+    private static WebService service;
     private WebService() {
     }
     public static WebService getService(){
+        if (service == null){
+            service = new WebService();
+        }
         return service;
     }
 
