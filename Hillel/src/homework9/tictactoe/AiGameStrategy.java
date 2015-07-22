38614c38974ef352a2d8f8fbdebb0784eda25339
yura <yurel83@ukr.net>
2015-07-22 22:14:41 +0300

    MyLinkedList

--- Hillel/src/homework9/tictactoe/AiGameStrategy.java
+++ Hillel/src/homework9/tictactoe/AiGameStrategy.java
@@ -3,7 +3,7 @@ package homework9.tictactoe;
 /**
  * Created by VSulevskiy on 19.07.2015.
  */
-public class GameLogic {
-    //default
+public interface AiGameStrategy {
+    public int aiMakeTurn(int[] field);
 
 }
---
+++ Hillel/src/homework9/tictactoe/RandomAiGameStrategy.java
@@ -0,0 +1,10 @@
+package homework9.tictactoe;
+
+import java.util.Random;
+
+/**
+ * Created by VSulevskiy on 19.07.2015.
+ */
+public class RandomAiGameStrategy implements AiGameStrategy {
+
+}
