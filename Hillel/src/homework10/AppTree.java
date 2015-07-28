a90596ebd3a187c1e374081b4f199e8f413d5562
yura <yurel83@ukr.net>
2015-07-28 16:25:05 +0300

    MyLinkedList

--- Hillel/src/homework10/AppTree.java
+++ Hillel/src/homework10/AppTree.java
@@ -17,15 +17,16 @@ public class AppTree {
         tree.add(19);
         tree.add(6);
 
-        System.out.println(tree.getMin());
-        System.out.println(tree.getMax());
-        System.out.println(tree.contains(1));
-        System.out.println(tree.contains(22));
-        System.out.println(tree.contains(23));
-        System.out.println("I'm max element in sub tree towards element 5 " + tree.maxSubTree(5));
-        System.out.println(" I'm min element in sub tree towards element 5 " +tree.minSubTree(5));
+        System.out.println("I'm min element in the tree " + tree.getMin());
+        System.out.println("I'm max element in the tree " + tree.getMax());
+        System.out.println("1 is contains in the tree " + tree.contains(1));
+        System.out.println("22 is contains in the tree " + tree.contains(22));
+        System.out.println("23 is contains in the tree " + tree.contains(23));
+        System.out.println("I'm max element in the sub tree towards element 5 " + tree.maxSubTree(5));
+        System.out.println("I'm min element in the sub tree towards element 5 " +tree.minSubTree(5));
         System.out.println("I'm result of floor method:" + tree.floor(5));
         System.out.println("I'm result of ceiling method: " + tree.ceiling(5));
+        System.out.println("Size of my tree: " + tree.getSize());
 
 
     }
--- Hillel/src/homework10/MyTreeSet.java
+++ Hillel/src/homework10/MyTreeSet.java
@@ -4,19 +4,22 @@ package homework10;
 /**
  * Created by yura on 27.07.15.
  */
-public class MyTreeSet{
+public class MyTreeSet {
     private Node root;
-    public void add(Object data){
+
+    public void add(Object data) {
         Node newNode = new Node(data);
         add(newNode);
     }
-    private void add(Node newNode){
-        if (root==null){
-            root =newNode;
+
+    private void add(Node newNode) {
+        if (root == null) {
+            root = newNode;
         } else {
             add(root, newNode);
         }
     }
+
     private void add(Node addTo, Node newNode) {
         Object addToData = addTo.getData();
         Object newNodeData = newNode.getData();
@@ -24,14 +27,14 @@ public class MyTreeSet{
         Comparable addToCompare = (Comparable) addToData;
         Comparable newNodeCompare = (Comparable) newNodeData;
         result = addToCompare.compareTo(newNodeCompare);
-        if (result >0){
-            if (addTo.getLeft()==null){
+        if (result > 0) {
+            if (addTo.getLeft() == null) {
                 addTo.setLeft(newNode);
             } else {
                 add(addTo.getLeft(), newNode);
             }
-        } else if(result < 0){
-            if (addTo.getRight()==null){
+        } else if (result < 0) {
+            if (addTo.getRight() == null) {
                 addTo.setRight(newNode);
             } else {
                 add(addTo.getRight(), newNode);
@@ -39,76 +42,83 @@ public class MyTreeSet{
         }
     }
 
-    public Object getMin(){
+    public Object getMin() {
         return getMin(root);
     }
-    public Object getMax(){
+
+    public Object getMax() {
         return getMax(root);
     }
-    private Object getMin(Node minNode){
-        while (minNode.getLeft()!=null){
-           minNode = minNode.getLeft();
-       }
+
+    private Object getMin(Node minNode) {
+        while (minNode.getLeft() != null) {
+            minNode = minNode.getLeft();
+        }
         return minNode.getData();
     }
 
-    private Object getMax(Node maxNode){
-        while (maxNode.getRight()!=null){
-           maxNode = maxNode.getRight();
-       }
+    private Object getMax(Node maxNode) {
+        while (maxNode.getRight() != null) {
+            maxNode = maxNode.getRight();
+        }
         return maxNode.getData();
     }
-    public boolean contains(Object o){
-       return contains(o, root)!=null;
+
+    public boolean contains(Object o) {
+        return contains(o, root) != null;
     }
 
-    private Node contains(Object o, Node inNode){
-        if(inNode==null){
+    private Node contains(Object o, Node inNode) {
+        if (inNode == null) {
             return null;
         }
         Comparable inNodeCompare = (Comparable) inNode.getData();
         Comparable oCompare = (Comparable) o;
-        if (inNodeCompare.compareTo(oCompare) > 0){
+        if (inNodeCompare.compareTo(oCompare) > 0) {
             return contains(o, inNode.getLeft());
-        } else if (inNodeCompare.compareTo(oCompare) < 0){
+        } else if (inNodeCompare.compareTo(oCompare) < 0) {
             return contains(o, inNode.getRight());
         } else {
             return inNode;
         }
     }
 
-    public Object floor(Object o){
+    public Object floor(Object o) {
         Node startNode = contains(o, root);
         return startNode.getRight().getData();
     }
 
-    public Object ceiling(Object o){
+    public Object ceiling(Object o) {
         Node startNode = contains(o, root);
         return startNode.getLeft().getData();
     }
 
-    public Object maxSubTree(Object o){
+    public Object maxSubTree(Object o) {
         Node subTree = contains(o, root);
-        if (subTree==null) {
+        if (subTree == null) {
             return null;
         }
         return getMax(subTree);
     }
 
-    public Object minSubTree(Object o){
+    public Object minSubTree(Object o) {
         Node subTree = contains(o, root);
-        if (subTree==null) {
+        if (subTree == null) {
             return null;
         }
         return getMin(subTree);
     }
 
-    @Override
-    public String toString() {
-      return null;
+    public int getSize(){
+        return getSize(root);
+    }
+    private int getSize(Node node){
+        if (node!=null){
+            return 1 + getSize(node.getLeft()) + getSize(node.getRight());
+        }
+        return 0;
     }
 }
-
  class Node {
     private Object data;
     private Node  left;
