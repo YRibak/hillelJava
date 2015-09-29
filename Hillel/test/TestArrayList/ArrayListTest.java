54b2362c59e37c7757c06270ad15ce8af51667bb
yura <yurel83@ukr.net>
2015-09-29 09:27:51 +0300

    attempt of testing

--- Hillel/test/TestArrayList/ArrayListTest.java
+++ Hillel/test/TestArrayList/ArrayListTest.java
@@ -2,67 +2,102 @@ package TestArrayList;
 import org.junit.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import static org.junit.Assert.*;
 /**
  * Created by yura on 28.09.15.
  */
 public class ArrayListTest {
-    List list;
+    List testedList;
+    Collection collection;
     @Before
     public void init(){
-        list = new ArrayList(Arrays.asList("one","two","three"));
+        testedList = new ArrayList(Arrays.asList("one","two","three"));
+        collection = new ArrayList(Arrays.asList("four","one"));
     }
+
     @Test
     public void sizeTest(){
-        assertEquals(3,list.size());
-        assertNotEquals(2, list.size());
+        assertEquals(3, testedList.size());
+        assertNotEquals(2, testedList.size());
     }
+
     @Test
     public void addTest(){
-        assertEquals(3,list.size());
-        list.add("four");
-        assertEquals(4,list.size());
-        assertEquals("four", list.get(3));
-        assertArrayEquals(new String []{"one", "two", "three", "four"}, list.toArray());
-        list.add(3,null);
-        assertTrue(5==list.size());
-        assertNull(list.get(3));
+        assertEquals(3, testedList.size());
+        testedList.add("four");
+        assertEquals(4, testedList.size());
+        assertEquals("four", testedList.get(3));
+        assertArrayEquals(new String[]{"one", "two", "three", "four"}, testedList.toArray());
+        testedList.add(3, null);
+        assertTrue(5 == testedList.size());
+        assertNull(testedList.get(3));
+    }
+
+    @Test
+    public void addAllTest(){
+        assertTrue(3==testedList.size());
+        assertArrayEquals(new String[]{"one", "two", "three"}, testedList.toArray());
+        testedList.addAll(collection);
+        assertTrue(5 == testedList.size());
+        assertArrayEquals(new String[]{"one", "two", "three", "four", "one"}, testedList.toArray());
     }
 
     @Test// (expected = NullPointerException.class)
     public void isEptyTest(){
-        assertTrue(0!=list.size());
-        list.clear();
-        assertTrue(0==list.size());
+        assertTrue(0 != testedList.size());
+        testedList.clear();
+        assertTrue(0 == testedList.size());
     }
 
     @Test
     public void clearTest(){
-        assertTrue(0!=list.size());
-        list.clear();
-        list.size();
-        assertTrue(0 == list.size());
+        assertTrue(0 != testedList.size());
+        testedList.clear();
+        assertTrue(0 == testedList.size());
     }
 
     @Test
     public void containsTest(){
-        assertTrue(list.contains("two"));
-        assertFalse(list.contains("test"));
+        assertTrue(testedList.contains("two"));
+        assertFalse(testedList.contains("test"));
+    }
+    @Test
+    public void containsAllTest(){
+        assertTrue(testedList.containsAll(new ArrayList(Arrays.asList("two", "one"))));
     }
     @Test
     public void setTest(){
-        assertEquals(3, list.size());
-        assertEquals("two", list.get(1));
-        list.set(1, "second");
-        assertEquals(3, list.size());
-        assertNotEquals("two", list.get(1));
-        assertEquals("second", list.get(1));
+        assertEquals(3, testedList.size());
+        assertEquals("two", testedList.get(1));
+        testedList.set(1, "second");
+        assertEquals(3, testedList.size());
+        assertNotEquals("two", testedList.get(1));
+        assertEquals("second", testedList.get(1));
     }
 
     @Test
     public void toArrayTest(){
-        assertArrayEquals(new String []{"one", "two", "three"}, list.toArray());
+        assertArrayEquals(new String[]{"one", "two", "three"}, testedList.toArray());
     }
 
+    @Test
+    public void removeTest(){
+        assertEquals(3, testedList.size());
+        assertTrue("two" == testedList.get(1));
+        testedList.remove("two");
+        assertEquals(2, testedList.size());
+        assertFalse("two" == testedList.get(1));
+    }
+
+    @Test
+    public void removeAllTest(){
+        assertTrue(3==testedList.size());
+        assertEquals("one", testedList.get(0));
+        testedList.removeAll(collection);
+        assertFalse(3==testedList.size());
+        assertTrue(2==testedList.size());
+        assertNotEquals("one",testedList.get(0));
+    }
 }
